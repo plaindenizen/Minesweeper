1c49f50e52aa6aa3a743002595e7521f5252ae0e
Jacob Barber <jakenbake12@live.com>
2024-04-09 15:01:34 +1200

    uuuuuuuhh

--- app.js
+++ app.js
@@ -59,3 +59,24 @@ const mineGrid = (arr) => {
     return arr.map((r,y) => r.map((c,x) => c === "-" ? getMine(getMine(x,y) : c)))
 
 }
+
+/*
+readline is a JS object (r1)
+*/
+
+//step 1 : include the radline module
+const readline = require('readline')
+
+//step 2 : create an instance of readline.interface
+const r1 = readline.Interface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+//step 3 : promt the user for their name
+rl.question('What is your name?', (name) => {
+    //step 4 : display a greeting
+    console.log(`Hello, ${name}!`)
+})
+//step 5 : close the readline interface.
+rl.close()
