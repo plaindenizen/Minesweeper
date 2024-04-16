4a781a397ab8cc4d43015de53b0a2bce51cafcaa
Jacob Barber <jakenbake12@live.com>
2024-04-16 13:11:41 +1200

    added readline note

--- app.js
+++ app.js
@@ -1,64 +1,72 @@
-//minesweeper
-/*
-create a function that takes a grid $ and - where each $ represents a mine and each - represents a mine free spot.
-return a n array where each $ is replaced by a digit indicating the number of mines immediately adjacent to the spot.
-*/
-
-/*
-minegrid([
-    ['-', '-', '-', '-', '-'],
-    ['-', '-', '-', '-', '-'],
-    ['-', '-', '$', '-', '-'],
-    ['-', '-', '-', '-', '-'],
-    ['-', '-', '-', '-', '-']
-])
-
-output => [
-    [
-    ['0', '0', '0', '0', '0'],
-    ['0', '1', '1', '1', '0'],
-    ['0', '1', '$', '1', '0'],
-    ['0', '1', '1', '1', '0'],
-    ['0', '0', '0', '0', '0']
-]
-*/
-
-//!SECTION Grid
-
-let x1 = [' ', ' ', ' ', ' ', ' ']
-let x2 = [' ', ' ', ' ', ' ', ' ']
-let x3 = [' ', ' ', ' ', ' ', ' ']
-let x4 = [' ', ' ', ' ', ' ', ' ']
-let x5 = [' ', ' ', ' ', ' ', ' ']
 
 
-y = [x1, x2, x3, x4, x5]
+// /*
+// readline is a JS object (r1)
+// */
 
-x3[2] = '$'
+// //step 1 : include the radline module
+// const readline = require('readline')
+
+// //step 2 : create an instance of readline.interface
+// const r1 = readline.Interface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
+
+// //step 3 : promt the user for their name
+// rl.question('What is your name?', (name) => {
+//     //step 4 : display a greeting
+//     console.log(`Hello, ${name}!`)
+// })
+// //step 5 : close the readline interface.
+// rl.close()
 
 
-console.log(y)
+//include the readline mudule module for command line input
+const readline = require ('readline')       //NOTE - 'require' is a global method that allows us to import external modules into nodeJS.
 
-const mineGrid = (arr) => {
-    //create a helper function that accesses the element position (y,x) in arr. If the y index is valid or exists
-    //I want to return arr[y][x]; otherwise return null (this will prevent accessing undefined elements and to avoid errors)
-    const checkAccess = (y,x) => arr[y] ? arr[y][x] : null
-    // create a helper function that calculates the number of adjacent mines around the cell at position (y,x)
-    const getMine = (y,x) => {
-        //NOTE - an array is constructed with elements representing the content of adjecent cells,
-        //we are going to use checkAccess for each of the eight possible position adjacent position.
-        return [
-            checkAccess(y+1, x),    //checks the cell directly below the current
-            checkAccess(y+1,x+1),   //checks the cell diagonally below and to the right of the current cell
-            checkAccess(y+1, x-1),  //checks the cell diagonally below and to the left of the current cell
-            checkAccess(y, x+1),    //checks the cell directly to the right of current cell.
-            checkAccess(y, x-1),    //checks the cell directly to the left of current cell.
-            checkAccess(y-1, x),    //checks the cell directly above current cell.
-            checkAccess(y-1,x+1),   //checks the cell diagonally above and to the right of the current cell.
-            checkAccess(y-1,x-1),   //checks the cell digonally above and to the left of the current cell.
-            //NOTE - construct an array of values that represent the contents of the cells adjacents, after the constructing of the arraywe will use filter method to keep only thsoe elements that are mines ('$').
-        ].filter(adjCell => adjCell === "$").length.toString()
-    }
-    return arr.map((r,y) => r.map((c,x) => c === "-" ? getMine(getMine(x,y) : c)))
+//create an interface for the input and output 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
+//function to generate a random number between min and max (inclusive)
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max-min +1)) + min
 }
+
+//the secret number to guess bewtween 1 and 100
+const secretNumber = getRandomInt(1,10)
+
+//Counter for number of attempts
+let attempts = 0
+
+console.log('guess the number! It/s between 1 and 10. You have unlimited attempts.')
+
+//create a function to start the game
+function guessNumber() {
+    rl.question('Enter your guess:', (answer)=> {
+        attempts += 1 //increment the number of attempts
+        const guess = parseInt(answer,10) //converting the input string to number
+
+        //check if the guess is correct, too or too low
+        if(guess === secretNumber) {
+            console.log(`Correct! the number was ${secretNumber}. It took you ${attempts}attempts.`)
+            rl.close()
+        } else if (guess < secretNumber) {
+            console.log('Too low!')
+            guessNumber()
+        } else if (guess > secretNumber) {
+            console.log("Too High!")
+            guessNumber()
+        } else {
+            console.log('Please enter a valid number')
+            guessNumber()
+        }
+    })
+}
+
+guessNumber()
